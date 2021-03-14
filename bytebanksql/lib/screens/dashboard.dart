fb88ff6eaea030bdbfd32e5d64bd906590410c01
JohnAzedo <jplimao12@gmail.com>
2021-03-14 08:14:27 -0300

    [bytebanksql] Create dashboard

--- bytebanksql/lib/screens/dashboard.dart
+++ bytebanksql/lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:bytebanksql/screens/contacts/list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 class Dashboard extends StatelessWidget {
@@ -10,20 +11,68 @@ class Dashboard extends StatelessWidget {
         title: Text('Dashboard'),
       ),
       body: Column(
+        mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.asset('images/logo.png'),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.people),
-        onPressed: () => {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => ContactList(),
-            ),
+          Row(
+            children: [
+              _FeatureItem('Transfers', Icons.monetization_on),
+              _FeatureItem('Transaction feed', Icons.description),
+            ],
           ),
-        },
+        ],
       ),
     );
   }
 }
+
+class _FeatureItem extends StatelessWidget {
+  final String name ;
+  final IconData icon;
+
+  _FeatureItem(this.name, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: (){
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ContactList(),
+              ),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                    icon,
+                    color: Colors.white,
+                    size: 24.0
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
